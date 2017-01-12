643c4b1885f194c275596df6701fd68adcbc2e88
Matti Ahinko <matti.ahinko@gofore.com>
2017-01-13 00:10:59 +0200

    Rename method and use path instead of key

--- element_scraper_hook_handler.py
+++ element_scraper_hook_handler.py
@@ -5,9 +5,9 @@ class ElementScraperHookHandler:
     def __init__(self, element_hooks):
         self.element_hooks = element_hooks
 
-    def get_element_of_key(self, key):
-        if key in self.element_hooks:
-            element_hook = self.element_hooks[key]
+    def get_element_of_hook(self, hook_path):
+        if hook_path in self.element_hooks:
+            element_hook = self.element_hooks[hook_path]
             page = urlopen(element_hook["url"])
             page_content = page.read())
             scraper = ElementScraper(page_content)
