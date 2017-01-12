1d23b4b7f4e05c0a8b51d0845f873801dfc8dd39
Matti Ahinko <matti.ahinko@gofore.com>
2017-01-13 00:17:01 +0200

    Change keys

--- element_scraper_hook_handler.py
+++ element_scraper_hook_handler.py
@@ -8,9 +8,10 @@ class ElementScraperHookHandler:
     def get_element_of_hook(self, hook_path):
         if hook_path in self.element_hooks:
             element_hook = self.element_hooks[hook_path]
-            page = urlopen(element_hook["url"])
+            page = urlopen(element_hook["element_url"])
             page_content = page.read())
             scraper = ElementScraper(page_content)
             return scraper.get_first_element_by_class(
-                element_hook["element_type"], element_hook["element_class"])
+                    element_hook["element_identifier_type"],
+                    element_hook["element_class"])
         return None
